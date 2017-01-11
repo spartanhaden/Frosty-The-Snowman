e4592cd32357243315a2471b9b95faeb69106679
Roger Gomes <rwgomes@mtu.edu>
2017-01-11 17:30:19 -0500

    Completed upto Navigating the ROS Filesystem

---
+++ RogerGomes29/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/rogergomes/Frosty-The-Snowman/RogerGomes29/catkin_ws/devel:/opt/ros/indigo"
+export LD_LIBRARY_PATH="/opt/ros/indigo/lib"
+export PWD="/home/rogergomes/Frosty-The-Snowman/RogerGomes29/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/rogergomes/Frosty-The-Snowman/RogerGomes29/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/rogergomes/Frosty-The-Snowman/RogerGomes29/catkin_ws/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
