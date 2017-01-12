e15eb520d9d7539c8a2e66d80465b2e788b33541
welcome2smruti <dash@mtu.edu>
2017-01-12 00:28:50 -0500

    understanding ROS topics setup

---
+++ smrutiprasad/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/robotics2/project1/Frosty-The-Snowman/smrutiprasad/catkin_ws/src
+# Build directory: /home/robotics2/project1/Frosty-The-Snowman/smrutiprasad/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(beginner_tutorials)
---
+++ smrutiprasad/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/robotics2/project1/Frosty-The-Snowman/smrutiprasad/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/robotics2/project1/Frosty-The-Snowman/smrutiprasad/catkin_ws/devel;/home/robotics2/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/robotics2/project1/Frosty-The-Snowman/smrutiprasad/catkin_ws/devel;/home/robotics2/catkin_ws/devel;/opt/ros/indigo" != "" else []
